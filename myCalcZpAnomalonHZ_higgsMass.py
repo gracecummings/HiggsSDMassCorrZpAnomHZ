436382cd2945d9c47ec50af33b8b05ac69662f49
gracecummings <graceelainecummings@gmail.com>
2020-04-29 15:32:24 +0200

    added different eta regions to multigraph

--- myCalcZpAnomalonHZ_higgsMass.py
+++ myCalcZpAnomalonHZ_higgsMass.py
@@ -39,57 +39,89 @@ if __name__=="__main__":
 
     
     #for h in hname:
-    h = tfiles[0].Get('hSDvpT_sfat_reco_uncorr')
+    h = tfiles[0].Get('hSDvpT_sfat_gen_corr')
+    h1d3 = tfiles[0].Get('hSDvpT_sfat_gen_corr_0eta1d3')
+    h2d5 = tfiles[0].Get('hSDvpT_sfat_gen_corr_1d3eta2d5')
     for i,tf in enumerate(tfiles):
         if i == 0:
-            h = tf.Get('hSDvpT_sfat_reco_uncorr')
+            h = tf.Get('hSDvpT_sfat_gen_corr')
+            h1d3 = tf.Get('hSDvpT_sfat_gen_corr_0eta1d3')
+            h2d5 = tf.Get('hSDvpT_sfat_gen_corr_1d3eta2d5')
         else:
-            h.Add(tf.Get('hSDvpT_sfat_reco_uncorr'))
-        print "Looking at file ",i
-        print "Total Events ",h.Integral()
+            h.Add(tf.Get('hSDvpT_sfat_gen_corr'))
+            h1d3.Add(tf.Get('hSDvpT_sfat_gen_corr_0eta1d3'))
+            h2d5.Add(tf.Get('hSDvpT_sfat_gen_corr_1d3eta2d5'))
     ptbins = h.GetNbinsX()
     sdbins = h.GetNbinsY()
     ptlist = np.zeros(ptbins)
-    avsdlist = np.zeros(ptbins)
+    avsdlist1d3 = np.zeros(ptbins)
+    avsdlist2d5 = np.zeros(ptbins)
 
     #Draw 2D plot
     p1.Draw()
     p1.cd()
     h.Draw("COLZ")
     h.GetXaxis().SetTitle("pT of AK8 jet closest to higgs mass")
-    h.GetYaxis().SetTitle("uncorr reco soft srop mass")
+    h.GetYaxis().SetTitle("corr gen soft srop mass")
     h.SetStats(0)
     tc.Modified()
 
     for i in range(ptbins):
         ptcen = h.GetXaxis().GetBinCenter(i+1)
-        events = 0
-        mass   = 0
-        massav = 0
+        events1d3 = 0
+        mass1d3   = 0
+        massav1d3 = 0
+
+        events2d5 = 0
+        mass2d5   = 0
+        massav2d5 = 0
+        
         for j in range(sdbins):
             sdval   = h.GetYaxis().GetBinCenter(j+1)
-            sdevnts = h.GetBinContent(i+1,j+1)
-            sdcont  = sdval*sdevnts
-            mass += sdcont
-            events += sdevnts
 
-        if events == 0:
-            massav = 0
+            sdevnts1d3 = h1d3.GetBinContent(i+1,j+1)
+            sdcont1d3  = sdval*sdevnts1d3
+            mass1d3 += sdcont1d3
+            events1d3 += sdevnts1d3
+
+            sdevnts2d5 = h2d5.GetBinContent(i+1,j+1)
+            sdcont2d5  = sdval*sdevnts2d5
+            mass2d5 += sdcont2d5
+            events2d5 += sdevnts2d5
+
+        if events1d3 == 0:
+            massav1d3 = 0
         else:
-            massav = mass/events
+            massav1d3 = mass1d3/events1d3
+
+        if events2d5 == 0:
+            massav2d5 = 0
+        else:
+            massav2d5 = mass2d5/events2d5
             
         ptlist[i] = ptcen
-        avsdlist[i] = massav
+        avsdlist1d3[i] = massav1d3
+        avsdlist2d5[i] = massav2d5
 
-    tg = ROOT.TGraph(ptbins,ptlist,avsdlist)
+    mg = ROOT.TMultiGraph()
+    tg1d3 = ROOT.TGraph(ptbins,ptlist,avsdlist1d3)
+    tg1d3.SetTitle("")
+    tg1d3.SetLineColor(ROOT.kBlue)
+    tg2d5 = ROOT.TGraph(ptbins,ptlist,avsdlist2d5)
+    tg2d5.SetTitle("")
+    tg2d5.SetLineColor(ROOT.kRed)
+    mg.Add(tg1d3)
+    mg.Add(tg2d5)
     tc.cd()
     p2.Draw()
     p2.cd()
-    tg.Draw()
-    tg.SetTitle("")
-    tg.GetXaxis().SetTitle("pT of AK8 jet closest to Higgs mass")
-    tg.GetYaxis().SetTitle("average SD mass")
-    tg.GetXaxis().SetLimits(ptlist[0],ptlist[-1]+h.GetXaxis().GetBinWidth(1)/2)
+    mg.Draw("AL")
+    mg.SetTitle("")
+    mg.GetXaxis().SetTitle("pT of AK8 jet closest to Higgs mass")
+    mg.GetYaxis().SetTitle("average SD mass")
+    mg.GetXaxis().SetLimits(ptlist[0],ptlist[-1]+h.GetXaxis().GetBinWidth(1)/2)
+    mg.SetMaximum(140)
+    mg.SetMinimum(50)
     tc.cd()
     p1.cd()
     
@@ -103,5 +135,5 @@ if __name__=="__main__":
     if not os.path.exists("mass_plots/"+savdir):
         os.makedirs("mass_plots/"+savdir)
     #pngname = "mass_plots/"+savdir+"/"+hname+"_deepdoubleb_optimization.png"
-    pngname = "mass_plots/"+savdir+"/hSDvpt_sfat_reco_uncorr.png"
+    pngname = "mass_plots/"+savdir+"/hSDvpt_sfat_gen_corr.png"
     tc.SaveAs(pngname)  
